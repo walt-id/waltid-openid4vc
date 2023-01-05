b19cd24c71bddcc8623e67447ac24896f43251fd
ahmed smiai <ahmedsmiai3@gmail.com>
2023-01-05 14:22:19 +0100

    update test file

--- src/main/test/test.js
+++ src/main/test/test.js
@@ -1,9 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
-    duration: '1m',
-    vus: 50,
+    duration: '.5m',
+    vus: 5,
+    iterations: 10,
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
         http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
@@ -11,7 +13,7 @@ export const options = {
 };
 
 export default function () {
-    const res = http.get('https://test.k6.io');
+    const res = http.get('https://test.k6.io/contacts.php');
     sleep(1);
 }
 export function handleSummary(data) {
