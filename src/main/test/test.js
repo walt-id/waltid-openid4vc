1bebabedfc3d16dc17628babf9a86f3fff2b4da9
ahmed smiai <ahmedsmiai3@gmail.com>
2023-01-05 14:10:01 +0100

    load-test workflow

--- src/main/test/test.js
+++ src/main/test/test.js
@@ -13,5 +13,12 @@ export const options = {
 export default function () {
     const res = http.get('https://test.k6.io');
     sleep(1);
+}
+export function handleSummary(data) {
+    console.log('Finished executing performance tests');
 
+    return {
+        'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
+        'summary.json': JSON.stringify(data), // and a JSON with all the details...
+    };
 }
