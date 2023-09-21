c5c48b390f7ca0933495f2fcfed743a44cfd4436
KBurgmann <kb@gatgetron.net>
2023-09-21 21:49:34 +0200

    Reformat OpenIDProvider

--- src/commonMain/kotlin/id/walt/oid4vc/providers/OpenIDProvider.kt
+++ src/commonMain/kotlin/id/walt/oid4vc/providers/OpenIDProvider.kt
@@ -14,139 +14,176 @@ import id.walt.oid4vc.requests.TokenRequest
 import id.walt.oid4vc.responses.*
 import io.ktor.http.*
 import kotlinx.datetime.Clock
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
 
-abstract class OpenIDProvider<S: AuthorizationSession>(
-  val baseUrl: String,
-): ISessionCache<S>, ITokenProvider {
-  abstract val metadata: OpenIDProviderMetadata
-  abstract val config: OpenIDProviderConfig
+abstract class OpenIDProvider<S : AuthorizationSession>(
+    val baseUrl: String,
+) : ISessionCache<S>, ITokenProvider {
+    abstract val metadata: OpenIDProviderMetadata
+    abstract val config: OpenIDProviderConfig
 
-  protected open fun createDefaultProviderMetadata() = OpenIDProviderMetadata(
-    issuer = baseUrl,
-    authorizationEndpoint = "$baseUrl/authorize",
-    pushedAuthorizationRequestEndpoint = "$baseUrl/par",
-    tokenEndpoint = "$baseUrl/token",
-    credentialEndpoint = "$baseUrl/credential",
-    batchCredentialEndpoint = "$baseUrl/batch_credential",
-    deferredCredentialEndpoint = "$baseUrl/credential_deferred",
-    jwksUri = "$baseUrl/jwks",
-    grantTypesSupported = setOf(GrantType.authorization_code, GrantType.pre_authorized_code),
-    requestUriParameterSupported = true,
-    subjectTypesSupported = setOf(SubjectType.public),
-    credentialIssuer = "$baseUrl/.well-known/openid-credential-issuer"
-  )
-
-  fun getCommonProviderMetadataUrl(): String {
-    return URLBuilder(baseUrl).apply {
-      pathSegments = listOf(".well-known", "openid-configuration")
-    }.buildString()
-  }
-
-  protected open fun generateToken(sub: String, audience: TokenTarget, tokenId: String? = null): String {
-    return signToken(audience, buildJsonObject {
-      put(JWTClaims.Payload.subject, sub);
-      put(JWTClaims.Payload.issuer, metadata.issuer)
-      put(JWTClaims.Payload.audience, audience.name)
-      tokenId?.let { put(JWTClaims.Payload.jwtID, it) }
-    })
-  }
-
-  protected open fun verifyAndParseToken(token: String, target: TokenTarget): JsonObject? {
-    if(verifyTokenSignature(target, token)) {
-      val payload = parseTokenPayload(token)
-      if(payload.keys.containsAll(setOf(JWTClaims.Payload.subject, JWTClaims.Payload.audience, JWTClaims.Payload.issuer)) &&
-          payload[JWTClaims.Payload.audience]!!.jsonPrimitive.content == target.name &&
-          payload[JWTClaims.Payload.issuer]!!.jsonPrimitive.content == metadata.issuer
-        ) {
-        return payload
-      }
-    }
-    return null
-  }
-
-  protected open fun generateAuthorizationCodeFor(session: S): String {
-    return generateToken(session.id, TokenTarget.TOKEN)
-  }
-
-  protected open fun validateAuthorizationCode(code: String): JsonObject? {
-    return verifyAndParseToken(code, TokenTarget.TOKEN)
-  }
-
-  protected abstract fun validateAuthorizationRequest(authorizationRequest: AuthorizationRequest): Boolean
-
-  abstract fun initializeAuthorization(authorizationRequest: AuthorizationRequest, expiresIn: Int): S
-  open fun processCodeFlowAuthorization(authorizationRequest: AuthorizationRequest): AuthorizationCodeResponse {
-    if(authorizationRequest.responseType != ResponseType.code.name)
-      throw AuthorizationError(authorizationRequest, AuthorizationErrorCode.invalid_request, message = "Invalid response type ${authorizationRequest.responseType}, for authorization code flow.")
-    val authorizationSession = getOrInitAuthorizationSession(authorizationRequest)
-    val code = generateAuthorizationCodeFor(authorizationSession)
-    return AuthorizationCodeResponse.success(code)
-  }
-
-  open fun processImplicitFlowAuthorization(authorizationRequest: AuthorizationRequest): TokenResponse {
-    if(!authorizationRequest.responseType.contains(ResponseType.token.name))
-      throw AuthorizationError(authorizationRequest, AuthorizationErrorCode.invalid_request, message = "Invalid response type ${authorizationRequest.responseType}, for implicit authorization flow.")
-    val authorizationSession = getOrInitAuthorizationSession(authorizationRequest)
-    return generateTokenResponse(authorizationSession, TokenRequest(GrantType.implicit, authorizationRequest.clientId))
-  }
-
-  protected open fun generateTokenResponse(session: S, tokenRequest: TokenRequest): TokenResponse {
-    return TokenResponse.success(
-      generateToken(session.id, TokenTarget.ACCESS),
-      "bearer"
+    protected open fun createDefaultProviderMetadata() = OpenIDProviderMetadata(
+        issuer = baseUrl,
+        authorizationEndpoint = "$baseUrl/authorize",
+        pushedAuthorizationRequestEndpoint = "$baseUrl/par",
+        tokenEndpoint = "$baseUrl/token",
+        credentialEndpoint = "$baseUrl/credential",
+        batchCredentialEndpoint = "$baseUrl/batch_credential",
+        deferredCredentialEndpoint = "$baseUrl/credential_deferred",
+        jwksUri = "$baseUrl/jwks",
+        grantTypesSupported = setOf(GrantType.authorization_code, GrantType.pre_authorized_code),
+        requestUriParameterSupported = true,
+        subjectTypesSupported = setOf(SubjectType.public),
+        credentialIssuer = "$baseUrl/.well-known/openid-credential-issuer"
     )
-  }
 
-  protected fun getVerifiedSession(sessionId: String): S? {
-    return getSession(sessionId)?.let {
-      if(it.isExpired) {
-        removeSession(sessionId)
-        null
-      } else {
-        it
-      }
+    fun getCommonProviderMetadataUrl(): String {
+        return URLBuilder(baseUrl).apply {
+            pathSegments = listOf(".well-known", "openid-configuration")
+        }.buildString()
     }
-  }
 
-  open fun processTokenRequest(tokenRequest: TokenRequest): TokenResponse {
-    val code = when(tokenRequest.grantType) {
-      GrantType.authorization_code -> tokenRequest.code ?: throw TokenError(tokenRequest, TokenErrorCode.invalid_grant, "No code parameter found on token request")
-      GrantType.pre_authorized_code -> tokenRequest.preAuthorizedCode ?: throw TokenError(tokenRequest, TokenErrorCode.invalid_grant, "No pre-authorized_code parameter found on token request")
-      else -> throw TokenError(tokenRequest, TokenErrorCode.unsupported_grant_type, "Grant type not supported")
+    protected open fun generateToken(sub: String, audience: TokenTarget, tokenId: String? = null): String {
+        return signToken(audience, buildJsonObject {
+            put(JWTClaims.Payload.subject, sub);
+            put(JWTClaims.Payload.issuer, metadata.issuer)
+            put(JWTClaims.Payload.audience, audience.name)
+            tokenId?.let { put(JWTClaims.Payload.jwtID, it) }
+        })
     }
-    val payload = validateAuthorizationCode(code) ?: throw TokenError(tokenRequest, TokenErrorCode.invalid_grant, "Authorization code could not be verified")
 
-    val sessionId = payload["sub"]!!.jsonPrimitive.content
-    val session = getVerifiedSession(sessionId) ?: throw TokenError(tokenRequest, TokenErrorCode.invalid_request, "No authorization session found for given authorization code, or session expired.")
-
-    return generateTokenResponse(session, tokenRequest)
-  }
-
-  fun getPushedAuthorizationSuccessResponse(authorizationSession: S) = PushedAuthorizationResponse.success(
-    requestUri = "urn:ietf:params:oauth:request_uri:${authorizationSession.id}",
-    expiresIn = authorizationSession.expirationTimestamp - Clock.System.now().epochSeconds
-  )
-
-  protected open fun getOrInitAuthorizationSession(authorizationRequest: AuthorizationRequest): S {
-    return when(authorizationRequest.isReferenceToPAR) {
-      true -> getPushedAuthorizationSession(authorizationRequest)
-      false -> initializeAuthorization(authorizationRequest, 600)
+    protected open fun verifyAndParseToken(token: String, target: TokenTarget): JsonObject? {
+        if (verifyTokenSignature(target, token)) {
+            val payload = parseTokenPayload(token)
+            if (payload.keys.containsAll(setOf(JWTClaims.Payload.subject, JWTClaims.Payload.audience, JWTClaims.Payload.issuer)) &&
+                payload[JWTClaims.Payload.audience]!!.jsonPrimitive.content == target.name &&
+                payload[JWTClaims.Payload.issuer]!!.jsonPrimitive.content == metadata.issuer
+            ) {
+                return payload
+            }
+        }
+        return null
     }
-  }
 
-  fun getPushedAuthorizationSession(authorizationRequest: AuthorizationRequest): S {
-    val session = authorizationRequest.requestUri?.let {
-      getVerifiedSession(
-        it.substringAfter("urn:ietf:params:oauth:request_uri:")
-      ) ?: throw AuthorizationError(authorizationRequest, AuthorizationErrorCode.invalid_request,"No session found for given request URI, or session expired")
-    } ?: throw AuthorizationError(authorizationRequest, AuthorizationErrorCode.invalid_request, "Authorization request does not refer to a pushed authorization session")
+    protected open fun generateAuthorizationCodeFor(session: S): String {
+        return generateToken(session.id, TokenTarget.TOKEN)
+    }
 
-    return session
-  }
+    protected open fun validateAuthorizationCode(code: String): JsonObject? {
+        return verifyAndParseToken(code, TokenTarget.TOKEN)
+    }
 
-  fun validateAccessToken(accessToken: String): Boolean {
-    return verifyAndParseToken(accessToken, TokenTarget.ACCESS) != null
-  }
+    protected abstract fun validateAuthorizationRequest(authorizationRequest: AuthorizationRequest): Boolean
+
+    abstract fun initializeAuthorization(authorizationRequest: AuthorizationRequest, expiresIn: Int): S
+    open fun processCodeFlowAuthorization(authorizationRequest: AuthorizationRequest): AuthorizationCodeResponse {
+        if (authorizationRequest.responseType != ResponseType.code.name)
+            throw AuthorizationError(
+                authorizationRequest,
+                AuthorizationErrorCode.invalid_request,
+                message = "Invalid response type ${authorizationRequest.responseType}, for authorization code flow."
+            )
+        val authorizationSession = getOrInitAuthorizationSession(authorizationRequest)
+        val code = generateAuthorizationCodeFor(authorizationSession)
+        return AuthorizationCodeResponse.success(code)
+    }
+
+    open fun processImplicitFlowAuthorization(authorizationRequest: AuthorizationRequest): TokenResponse {
+        if (!authorizationRequest.responseType.contains(ResponseType.token.name))
+            throw AuthorizationError(
+                authorizationRequest,
+                AuthorizationErrorCode.invalid_request,
+                message = "Invalid response type ${authorizationRequest.responseType}, for implicit authorization flow."
+            )
+        val authorizationSession = getOrInitAuthorizationSession(authorizationRequest)
+        return generateTokenResponse(authorizationSession, TokenRequest(GrantType.implicit, authorizationRequest.clientId))
+    }
+
+    protected open fun generateTokenResponse(session: S, tokenRequest: TokenRequest): TokenResponse {
+        return TokenResponse.success(
+            generateToken(session.id, TokenTarget.ACCESS),
+            "bearer"
+        )
+    }
+
+    protected fun getVerifiedSession(sessionId: String): S? {
+        return getSession(sessionId)?.let {
+            if (it.isExpired) {
+                removeSession(sessionId)
+                null
+            } else {
+                it
+            }
+        }
+    }
+
+    open fun processTokenRequest(tokenRequest: TokenRequest): TokenResponse {
+        val code = when (tokenRequest.grantType) {
+            GrantType.authorization_code -> tokenRequest.code ?: throw TokenError(
+                tokenRequest = tokenRequest,
+                errorCode = TokenErrorCode.invalid_grant,
+                message = "No code parameter found on token request"
+            )
+
+            GrantType.pre_authorized_code -> tokenRequest.preAuthorizedCode ?: throw TokenError(
+                tokenRequest = tokenRequest,
+                errorCode = TokenErrorCode.invalid_grant,
+                message = "No pre-authorized_code parameter found on token request"
+            )
+
+            else -> throw TokenError(tokenRequest, TokenErrorCode.unsupported_grant_type, "Grant type not supported")
+        }
+        val payload = validateAuthorizationCode(code) ?: throw TokenError(
+            tokenRequest = tokenRequest,
+            errorCode = TokenErrorCode.invalid_grant,
+            message = "Authorization code could not be verified"
+        )
+
+        val sessionId = payload["sub"]!!.jsonPrimitive.content
+        val session = getVerifiedSession(sessionId) ?: throw TokenError(
+            tokenRequest = tokenRequest,
+            errorCode = TokenErrorCode.invalid_request,
+            message = "No authorization session found for given authorization code, or session expired."
+        )
+
+        return generateTokenResponse(session, tokenRequest)
+    }
+
+    fun getPushedAuthorizationSuccessResponse(authorizationSession: S) = PushedAuthorizationResponse.success(
+        requestUri = "urn:ietf:params:oauth:request_uri:${authorizationSession.id}",
+        expiresIn = authorizationSession.expirationTimestamp - Clock.System.now().epochSeconds
+    )
+
+    protected open fun getOrInitAuthorizationSession(authorizationRequest: AuthorizationRequest): S {
+        return when (authorizationRequest.isReferenceToPAR) {
+            true -> getPushedAuthorizationSession(authorizationRequest)
+            false -> initializeAuthorization(authorizationRequest, 600)
+        }
+    }
+
+    fun getPushedAuthorizationSession(authorizationRequest: AuthorizationRequest): S {
+        val session = authorizationRequest.requestUri?.let {
+            getVerifiedSession(
+                it.substringAfter("urn:ietf:params:oauth:request_uri:")
+            ) ?: throw AuthorizationError(
+                authorizationRequest,
+                AuthorizationErrorCode.invalid_request,
+                "No session found for given request URI, or session expired"
+            )
+        } ?: throw AuthorizationError(
+            authorizationRequest,
+            AuthorizationErrorCode.invalid_request,
+            "Authorization request does not refer to a pushed authorization session"
+        )
+
+        return session
+    }
+
+    fun validateAccessToken(accessToken: String): Boolean {
+        return verifyAndParseToken(accessToken, TokenTarget.ACCESS) != null
+    }
 
 }
